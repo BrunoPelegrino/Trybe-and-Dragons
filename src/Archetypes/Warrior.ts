8e158fd01f6f46f16b4c2f50911ef855bff6c2a7
Bruno Pelegrino <pelegrinobruno13@gmail.com>
2022-11-07 14:11:30 -0300

    feat: req5 done, Mage,Necromance,Range,Warrior created

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,21 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Warrior extends Archetype {
+  private static _createdArcheType = 0;
+  private _energyType: EnergyType;
+
+  constructor(name: string) {
+    super(name);
+    Warrior._createdArcheType += 1;
+    this._energyType = 'stamina';
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    return Warrior._createdArcheType;
+  }
+}
